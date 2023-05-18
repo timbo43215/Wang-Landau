cd4f8c2ebe70b3fa1c4122420e3eea72c8fb10b7
Tim Stack <tstack43215@yahoo.com>
2023-05-18 10:12:47 -0500

    Initial Commit

---
+++ Wang Landau/Wang_LandauApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wang_LandauApp.swift
+//  Wang Landau
+//
+//  Created by IIT PHYS 440 on 5/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Wang_LandauApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
